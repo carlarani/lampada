ba234cb6ff425e0c46d8e6e24fea5fbf19413b86
Carla Rani <carla.rani@gmail.com>
2022-11-11 19:41:20 -0300

    Add alteração de background

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function Ligar() {
-    document.getElementById("lampada").src="ligada.png";
-    document.getElementById("ligar").style.visibility="hidden";
-    document.getElementById("desligar").style.visibility="visible";
-  }
+  document.getElementById("lampada").src = "ligada.png";
+  document.getElementById("ligar").style.visibility = "hidden";
+  document.getElementById("desligar").style.visibility = "visible";
+  var back = document.getElementsByTagName("body");
+  back[0].style.backgroundColor = "white";
 
-  function Desligar() {
-    document.getElementById("lampada").src="desligada.png";
-    document.getElementById("desligar").style.visibility="hidden";
-    document.getElementById("ligar").style.visibility="visible";
-  }
+}
+
+function Desligar() {
+  document.getElementById("lampada").src = "desligada.png";
+  document.getElementById("desligar").style.visibility = "hidden";
+  document.getElementById("ligar").style.visibility = "visible";
+  var back = document.getElementsByTagName("body");
+  back[0].style.backgroundColor = "black";
+
+}
